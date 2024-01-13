c28c05c6fe395bf172906b9bc7d6a3fef3a84fad
Leonardo Perez <83437994+Ldltorre14@users.noreply.github.com>
2024-01-13 01:52:58 -0600

    Create main.js

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var element = document.getElementById("github-icon")
+    element.addEventListener("click", function(){
+        window.open("https://github.com/Ldltorre14","_blank")
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function(){
+    var element = document.getElementById("linkedin-icon")
+    element.addEventListener("click", function(){
+        window.open("https://www.linkedin.com/in/leonardo-octavio-perez-de-la-torre-164a6b23b",
+        "_blank")
+    })
+})
